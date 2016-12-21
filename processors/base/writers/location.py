0d74acd3ef4edd5083a995d7a07ecad71759369d
Felipe Vieira <felipe29vieira@gmail.com>
2016-12-21 18:09:04 -0300

    add must-link and cannot-link relations when clustering countries

--- processors/base/writers/location.py
+++ processors/base/writers/location.py
@@ -13,6 +13,9 @@ import datetime
 import jellyfish
 import numpy
 import distance
+import pycountry
+import os
+import distance
 # from .. import helpers
 logger = logging.getLogger(__name__)
 
@@ -77,40 +80,90 @@ def write_location(conn, location, source_id, trial_id=None):
     return object['id']
 
 def build_country_clusters():
+    MAX_DISTANCE = 0.9
+    MIN_DISTANCE = 0.01
     # An inner function that computs the Jaro distance between two countries
-    def _d(coord):
-        i, j = coord
-        return 1 - jellyfish.jaro_distance(countries[i], countries[j])
+    def _cluster_distance(coord):
+        # A simple cannot-link relantionship that prevents canonical clusters form being merged
+        def cannot_link(coord):
+            i, j = coord
+            ans = False
+            if countries[i] in canonical_names and countries[j] in canonical_names:
+                ans = True
+            return ans
+        
+        # A simple must-link relationship that forces merges between canonical forms and abbreviations 
+        def must_link(coord):
+            i, j = coord
+            ans = False
+            if countries[i] in acronyms:
+                if len(countries[i]) == 2: 
+                    ans = pycountry.countries.get(alpha_2=countries[i]).name == countries[j]
+                else: 
+                    ans = pycountry.countries.get(alpha_3=countries[i]).name == countries[j]
+            elif countries[j] in acronyms:
+                if len(countries[j]) == 2: 
+                    ans = pycountry.countries.get(alpha_2=countries[j]).name == countries[i]
+                else:
+                    ans = pycountry.countries.get(alpha_3=countries[j]).name == countries[i]
+            return ans
+        
+        # Setting the clustering distances according to must-link and cannot-link relationships
+        if not (cannot_link(coord) or must_link(coord)):
+            i, j = coord
+            #temp =   1 - jellyfish.jaro_distance(countries[i], countries[j])
+            temp = distance.nlevenshtein(countries[i], countries[j])
+        elif cannot_link(coord):
+            temp = MAX_DISTANCE
+        elif must_link(coord):
+            temp = MIN_DISTANCE
+        return temp
+
 
     # Read an normalize country names
-    training_data = open('/home/felipevieira/Área de Trabalho/data for training.txt', 'r')
+    training_data = open(os.path.join(os.path.dirname(__file__), 'data for training.txt'), 'r')
     countries = [unicode(country.strip(), encoding="utf-8") for country in training_data.readlines()]
-    
+
+    canonical_names = []
+    acronyms = []
+
+    for c in pycountry.countries:
+        canonical_names.append(c.name)
+        acronyms.append(c.alpha_3)
+        acronyms.append(c.alpha_2)
+   
     # Compute the triangle matrix used by the hierarchical clustering
     indexes_triangle_matrix = numpy.triu_indices(len(countries), 1)
-    distances_triangle_matrix = numpy.apply_along_axis(_d, 0, indexes_triangle_matrix)
+    distances_triangle_matrix = numpy.apply_along_axis(_cluster_distance, 0, indexes_triangle_matrix)
 
     # Compute the country likage and extract the clusters (249 is the number of acknowledged countries according to 3166-1)
-    country_linkage_matrix = linkage(distances_triangle_matrix, method="single")
-    flat_custers = fcluster(country_linkage_matrix, 0.15, criterion='distance')
+    country_linkage_matrix = linkage(distances_triangle_matrix, method='complete')
+    flat_custers = fcluster(country_linkage_matrix, 0.4, criterion='distance')
 
     clusters = {}
     for i in range(len(flat_custers)):
         clusters[flat_custers[i]] = clusters.get(flat_custers[i], []) + [countries[i]] 
 
-    for cluster, cluster_items in clusters.iteritems():
-        print(cluster_items)
+    # for cluster, cluster_items in clusters.iteritems():
+    #     if len(cluster_items) > 1:
+    #         print(cluster_items)
 
     return clusters
 
-def predict_canonical_country_name(country):
+def predict_canonical_country_name(country,  retrain=False):
     # Search for the country on the pre-built clusters
     for cluster_id, cluster_items in build_country_clusters().iteritems():
         if country in cluster_items:
             # Contry found. Return it and log the successful normalization
-            return
+            return list(set(cluster_items) & set([c.name for c in pycountry.countries]))
     # Contry not found. Add new location to database before retrain the model
-    predict_canonical_country_name(country)
+    if not retrain:
+        training_data = open(os.path.join(os.path.dirname(__file__), 'data for training.txt'), 'a')
+        training_data.write("\n%s" % country)
+        training_data.close()
+        return predict_canonical_country_name(country, retrain=True)
+    else:
+        print("not normalized. log and add to database")
 
 # if __name__ == '__main__':
-#     predict_canonical_country_name("Cina")
+#     print(predict_canonical_country_name("Bresil"))
